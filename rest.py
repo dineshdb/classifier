7c888ea148388c84b275361cbefcc8e17f6c3251
Yogesh Rai <ygsh.spcry5@gmail.com>
2018-07-30 01:19:50 +0545

    entityRecognition added

--- rest.py
+++ rest.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
+from textblob import TextBlob
+import nltk
 from DNN_multilabel import eval1
 from CNN_binary import eval2
 
 app = Flask(__name__)
 api = Api(app)
 
-class multilabel(Resource):
-    def get(self,name):
-        return eval1.categories[eval1.np.argmax(eval1.model.predict([eval1.get_tf_record(name)]))]
-		
-class binary(Resource):
+class classifyIntent(Resource):
     def get(self,name):
         x_raw = [name] #input sentence
         x_test = eval2.np.array(list(eval2.vocab_processor.transform(x_raw)))
@@ -24,13 +22,27 @@ class binary(Resource):
             batch_predictions = eval2.sess.run(eval2.predictions, {eval2.input_x: x_test_batch, eval2.dropout_keep_prob: 1.0})
             if(batch_predictions==[0]):
                  result = "random intent"
+                 return result
+                 
             else:
-                 result = "functional intent"
-        
-        return result
+                 return eval1.categories[eval1.np.argmax(eval1.model.predict([eval1.get_tf_record(name)]))]
+     
+class entityExtract(Resource):
+    def get(self,name):
+        nouns = []
+        for word,pos in nltk.pos_tag(nltk.word_tokenize(str(name))):
+                 if (pos == 'NN' or pos == 'NN' or pos == 'NNS' or pos == 'NN'):
+                     nouns.append(word)
+        blob = TextBlob(name)
+        print(blob.noun_phrases)
+        print(nouns)
+        return [blob.noun_phrases,nouns]
+		
+
     
-api.add_resource(binary, '/binary/<name>') # Route_1
-api.add_resource(multilabel, '/multilabel/<name>') # Route_2
+api.add_resource(classifyIntent, '/classifyIntent/<name>') # Route_1
+api.add_resource(entityExtract, '/entityExtract/<name>') # Route_2
+
 
 
 if __name__ == '__main__':
